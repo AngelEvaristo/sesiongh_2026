fabd74368b1989b248cb5fc153e2049d92010406
Angel EVaristo <aevaristoa2@hotmail.com>
2026-03-16 22:29:07 -0500

    ajuste

--- MyMinimalApi/Program.cs
+++ MyMinimalApi/Program.cs
@@ -1,7 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hola Mundo!");
+// SECRET DE PRUEBA - GHAS Secret Scanning (fake)
+// Este valor es intencionalmente mal formado para activar detecciones de scanner
+const string GITHUB_PAC_TOKEN = "ghp_qA1b2C3d4E5f6G7h8I9j0K1l2M3n4O5p6Q7r8";
+const string GCP_SECRET_KEY_ID = "1234567890abcdef1234567890abcdef12345678";
+
+app.MapGet("/", () => new {
+    Message = "Hola Mundo!",
+    SecretScanProof = new {
+        githubToken = GITHUB_PAC_TOKEN,
+        gcpKeyId = GCP_SECRET_KEY_ID
+    }
+});
 
 app.Run();
 
